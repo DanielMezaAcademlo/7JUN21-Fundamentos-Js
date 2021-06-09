770a4248741dec8d8de9c769b3a6351641723b92
DanielMezaAcademlo <danielmezauaeh@gmail.com>
2021-06-08 19:56:26 -0500

    Ejercicio casi resuelto

--- js/table.js
+++ js/table.js
@@ -1,3 +1,5 @@
+let table = document.querySelector("#users-table tbody");
+
 const users = [
   {
     name: "Erik",
@@ -5,7 +7,8 @@ const users = [
     email: "erik@academlo.com",
     social: [
       { name: "facebook", url: "facebook/erik" },
-      { name: "twitter", url: "twitter/erik" }
+      { name: "twitter", url: "twitter/erik" },
+      { name: "youtube", url: "youtube/erik" }
     ],
     gender: "Male"
   },
@@ -13,10 +16,7 @@ const users = [
     name: "Georg",
     age: 33,
     email: "georg@academlo.com",
-    social: [
-      { name: "facebook", url: "facebook/georg" },
-      { name: "twitter", url: "twitter/georg" }
-    ],
+    social: [{ name: "facebook", url: "facebook/georg" }],
     gender: "Male"
   },
   {
@@ -35,7 +35,7 @@ const users = [
     email: "oscar@academlo.com",
     social: [
       { name: "facebook", url: "facebook/oscar" },
-      { name: "twitter", url: "twiter/oscar" }
+      { name: "github", url: "github/erik" }
     ],
     gender: "Male"
   },
@@ -51,4 +51,36 @@ const users = [
   }
 ];
 
-function createTable() {}
+function createTable(usersList) {
+  for (let i = 0; i < usersList.length; i++) {
+    let userEmail = usersList[i].email;
+    if (userEmail.endsWith("@academlo.com")) {
+      //COMO VAMOS A ACCEDER A EL CAMPO SOCIAL
+      for (let index = 0; index < usersList[i].social.length; index++) {
+        console.log(usersList[i].email);
+        console.log(
+          ` ${usersList[i].name} tiene las siguientes redes sociales ${usersList[i].social[index].url}`
+        );
+      }
+      //CREAR FILAS EN LA TABLA USANDO CREATE ELEMENT
+      // let newRow = document.createElement("tr");
+      // table.appendChild(newRow);
+      // let userName = document.createElement("td");
+      // userName.innerText = usersList[i].name;
+      // let userEmail = document.createElement("td");
+      // userEmail.innerText = usersList[i].email;
+      // newRow.appendChild(userName);
+      // newRow.appendChild(userEmail);
+      //CREAR FILAS EN LA TABLA CON INNERHTML
+      // table.innerHTML += `<tr>
+      //                     <td>${usersList[i].name}</td>
+      //                     <td>${usersList[i].email}</td>
+      //                     <td>${usersList[i].age}</td>
+      //                     <td>${usersList[i].gender}</td>
+
+      //                   </tr>`;
+    }
+  }
+}
+
+createTable(users);
